1b22fc7e7f4a63a6394027f683f640b41f4a0455
Shubham Singh <imshubhamsingh97@gmail.com>
2021-11-30 23:52:28 +0530

    feat: working sveltkit

--- examples/with-sveltekit/src/routes/index.svelte
+++ examples/with-sveltekit/src/routes/index.svelte
@@ -1,25 +1,43 @@
 <script>
-    
-    import Player from '@vimeo-player/svelte';
-    let paused = true;
-    let ref;
-    $: {
-        console.log("paused", paused)
-    }
+  import Player from '@vimeo-player/svelte';
+  let paused = true;
+  let ref; // player ref
+  async function seek(value) {
+    if (!ref.getDuration) return;
+    const totalTime = await ref.getDuration();
+    const getCurrentTime = await ref.getCurrentTime();
+    await ref.seekTo(Math.min(Math.max(getCurrentTime + value, 0), totalTime));
+  }
 </script>
-<Player 
-video="323783503" 
-paused={paused}
-bid:ref={ref}
-on:ready={({detail}) => console.log("ready", detail)} 
-on:play={({detail}) => console.log("play", detail)}
-on:pause={({detail}) => console.log("pause", detail)}
+
+<h1>Welcome to SvelteKit</h1>
+<Player
+  video="323783503"
+  {paused}
+  bind:ref
+  on:ready={({ detail }) => console.log('ready', detail)}
+  on:play={({ detail }) => console.log('play', detail)}
+  on:pause={({ detail }) => console.log('pause', detail)}
 />
 
 <div>
-    <button on:click={() => {paused=true}}>Pause ⏸</button>
-    <button on:click={() => {paused=false}}>Play ▶️</button>
-  </div>
+  <button on:click={() => seek(-10)}>-10s ⏪</button>
+  <button on:click={() => seek(-5)}>-5s ◀️</button>
+  <button on:click={() => seek(5)}>+5s ▶️</button>
+  <button on:click={() => seek(10)}>+10s ⏩</button>
+</div>
 
-<h1>Welcome to SvelteKit</h1>
+<div>
+  <button
+    on:click={() => {
+      paused = true;
+    }}>Pause ⏸</button
+  >
+  <button
+    on:click={() => {
+      paused = false;
+    }}>Play ▶️</button
+  >
+</div>
+
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
